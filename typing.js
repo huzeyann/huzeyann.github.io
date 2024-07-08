658626ad2da282c7749113492c1ea67ec8df3444
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 02:33:27 +0000

    garrally [ci skip]

--- typing.js
+++ typing.js
@@ -1,13 +1,1 @@
-window.onload = function() {
-  new Typed('#typing-text', {
-    strings: ["seeing an image without looking", "looking an image without thinking"],
-    typeSpeed: 50,
-    // startDelay: 100,
-    backDelay: 2000,
-    fadeOut: true,
-    backSpeed: 40,
-    smartBackspace: true,
-    loop: true,
-    showCursor: false
-  });
-};
+window.onload=function(){new Typed("#typing-text",{strings:["seeing an image without looking","looking an image without thinking"],typeSpeed:50,backDelay:2e3,fadeOut:!0,backSpeed:40,smartBackspace:!0,loop:!0,showCursor:!1})};
